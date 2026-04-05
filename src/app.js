c87e2d019f65f82f320f5e99e2b678c2823bd70b
mdjoyanes-lgtm <mdjoyanes@gmail.com>
2026-04-05 18:36:05 +0000

    ended

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
 import "bootstrap";
 import "./style.css";
 
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "destroyed", "broke", "lost"];
+  let what = ["my homework", "my phone", "my car"];
+  let when = ["before class", "when I was sleeping", "during lunch"];
+
+  function randomElement(array) {
+    return array[Math.floor(Math.random() * array.length)];
+  }
+
+  let excuse =
+    randomElement(who) + " " +
+    randomElement(action) + " " +
+    randomElement(what) + " " +
+    randomElement(when);
+
+  document.querySelector("#excuse").innerHTML = excuse;
 };
